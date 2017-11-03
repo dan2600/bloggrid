cd641cca93f09cc2122f08eccbaad63fd0f7eb2a
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-11-03 15:57:33 -0400

    additional updates

--- index.js
+++ index.js
@@ -4,47 +4,40 @@ var compression = require('compression');
 var express = require('express');
 var app = express();
 var path = require("path");
-var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
-var request = require('request');
-const FeedMe = require('feedme');
-
+var FeedMe = require('feedme');
 //Setup
 app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/', express.static(path.join(__dirname, "public")));
-//app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
 var http = require('http').Server(app);
-var httprq = require('http');
-//var io = require('socket.io')(http);
+var rssreqest = require('http');
 var port = process.env.PORT || 8080;
 
-
-
+//RSS Feed to JSON Parser
 app.get('/rssfeed', function(req, res) {
-
-    httprq.get('http://www.vh1.com/news/feed/', (ror) => {
-
+    rssreqest.get('http://www.vh1.com/news/feed', (ror) => {
         var parser = new FeedMe(true);
-        ror.pipe(parser);
+        parser.on('error', (d) => {
+            console.log("RSS Feed read bad! Ugh!");
+            res.set('content-type', 'text/json');
+            res.send({"error":true});
+        });
         parser.on('end', () => {
             res.set('content-type', 'text/json');
             res.send(parser.done());
         });
+        ror.pipe(parser);
     });
-
-
 });
-
 
 app.get('/*', function(req, res) {
     res.status(404);
+    res.send("Error 404");
 });
 
 http.listen(port, function() {
