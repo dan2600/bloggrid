326ebe1d7792ce4d366f69741b3d55b3b20ac0b0
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-12-05 16:37:27 -0500

    code clean up

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-"use strict";
+//Main File
+'use strict';
 //Dependencies
-var compression = require("compression");
-require("marko/node-require");
-var express = require("express");
-var markoExpress = require("marko/express");
-var page = require("./page");
+var compression = require('compression');
+require('marko/node-require');
+var express = require('express');
+var markoExpress = require('marko/express');
+var page = require('./page');
 var app = express();
-var path = require("path");
-var FeedMe = require("feedme");
-var http = require("http").Server(app);
-var rssreqest = require("http");
-var secure = require("express-force-https");
-var ontime = require("ontime");
+var path = require('path');
+var http = require('http').Server(app);
+var secure = require('express-force-https');
+var ontime = require('ontime');
 var port = process.env.PORT || 8080;
-var newsJSON = "";
-var retryCount = 0;
-const config = require("./config");
-require("datejs");
+const config = require('./config');
+require('datejs');
+var clearTemp = require('./lib/clearTemp');
+var makeSmall = require('./lib/makeSmall');
+var parseJSONitems = require('./lib/parseJSONitems');
+var updateNewsFeed;
 
 //Setup
 app.use(compression());
-app.use(express.static(path.join(__dirname, "public"), { maxage: "30d" }));
+app.use(express.static(path.join(__dirname, 'public'), { maxage: '30d' }));
 app.use(markoExpress());
 app.use(secure);
-app.disable("x-powered-by");
-const directory = "./public/images";
-var clearTemp = require("./lib/clearTemp");
-var makeSmall = require("./lib/makeSmall");
-var parseJSONitems = require("./lib/parseJSONitems");
-var updateNewsFeed;
+app.disable('x-powered-by');
+const directory = './public/images';
+var newsJSON = '';
 
+//Get RSS information
 if (!process.env.REDIS_URL) {
-  console.log("development mode");
-  updateNewsFeed = require("./lib/updateNewsFeedNoCache");
+  console.log('development mode');
+  updateNewsFeed = require('./lib/updateNewsFeedNoCache');
   updateNewsFeed(directory, json => {
     newsJSON = json;
   });
 } else {
-  console.log("production mode");
-  updateNewsFeed = require("./lib/updateNewsFeed");
+  console.log('production mode');
+  updateNewsFeed = require('./lib/updateNewsFeed');
   updateNewsFeed(directory, (json, error) => {
     newsJSON = json;
     if (error) {
@@ -48,16 +47,18 @@ if (!process.env.REDIS_URL) {
     }
   });
 }
+
+//Schedule Hourly RSS updates
 ontime(
   {
-    cycle: ["00:00"]
+    cycle: ['00:00']
   },
   function(ot) {
-    console.log("Checking for news feed updates");
+    console.log('Checking for news feed updates');
     updateNewsFeed(directory, (json, error) => {
       newsJSON = json;
       if (error) {
-        console.log("error loading RSS, retrying in 1 minute");
+        console.log('error loading RSS, retrying in 1 minute');
         setTimeout(function() {
           updateNewsFeed(directory, (json, error) => {
             newsJSON = json;
@@ -70,13 +71,15 @@ ontime(
   }
 );
 
-app.get("/*", function(req, res) {
+//Routes
+app.get('/*', function(req, res) {
   res.marko(page, {
     items: newsJSON,
     pageData: config.pageData
   });
 });
 
+//Server
 http.listen(port, function() {
-  console.log(`INFO: listening on ${port}`);
+  console.log('INFO: listening on ' + port);
 });
